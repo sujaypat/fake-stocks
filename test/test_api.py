da5a7e4900bee33033f216dab03c7eb61b214883
Sujay <cheeselover724@gmail.com>
2017-11-10 15:34:19 -0600

    init commit

---
+++ test/test_api.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestAPIMethods(unittest.TestCase):
+
+    def test_index(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_dashboard(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
